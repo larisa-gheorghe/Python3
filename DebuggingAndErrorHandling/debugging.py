b630ab13c4573094a923b7c628028d8e3312872f
Larisa Zaharia <larisa.camelia.gheorghe@gmail.com>
2024-12-15 17:52:38 +0200

    Debugging and Error Handling

---
+++ DebuggingAndErrorHandling/debugging.py
@@ -0,0 +1,30 @@
+# FIRST EXAMPLE:
+# import pdb
+# first = "First"
+# second = "Second"
+# pdb.set_trace()
+# result = first + second
+# third = "Third"
+# result += third
+# print(result)
+
+def add_numbers(a, b, c, d):
+    import pdb; pdb.set_trace() 
+
+    return a + b + c + d
+add_numbers(1,2,3,4)
+
+# ===================
+# NOTES
+# ===================
+# import pdb
+# pdb.set_trace()
+
+# Also commonly on one line:
+# import pdb; pdb.set_trace()
+
+# Common PDB Commands:
+# l (list)
+# n (next line)
+# p (print)
+# c (continue - finishes debugging)
