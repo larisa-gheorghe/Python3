7fdab16f03e0cc7c1d556ed0dc05b7d96b9bc588
Larisa Zaharia <larisa.camelia.gheorghe@gmail.com>
2024-12-15 17:51:10 +0200

    Lists

---
+++ Lists/list_comprehension.py
@@ -0,0 +1,21 @@
+# ex 1
+# names = ["Elie", "Tim", "Matt"]
+# answer = [name[0] for name in names]
+# nums = [1,2,3,4,5,6]
+# answer2 = [num for num in nums if num % 2 == 0]
+
+# ex 2
+# answer = [num for num in [1, 2, 3, 4] if num in [3, 4, 5, 6]]
+# answer2 = [name[::-1].lower() for name in ["Elie", "Tim", "Matt"]]
+
+# ex 3
+# answer =  [num for num in range(1, 101) if num % 12 == 0]
+
+# ex 4
+# answer = [char for char in "amazing" if char not in "aeiou"]
+
+# ex 5
+# answer = [[num for num in range(3)] for num in range(3)]
+
+# ex 6
+answer = [[i for i in range(10)] for num in range(10)]
---
+++ Lists/list_iteration.py
@@ -0,0 +1,8 @@
+sounds = ["super", "cali", "fragil", "istic", "expi", "ali", "docious"]
+
+# Define your code below:
+result = ""
+for sound in sounds:
+    result += sound
+result = result.upper()
+print(result)
